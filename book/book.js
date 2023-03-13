66a87a777c1c9d16b3c43433c1a036c80a84d9d1
dohakim0205 <8772158@naver.com>
2023-03-13 10:38:55 +0900

    rename index

---
+++ book/book.js
@@ -0,0 +1,96 @@
+// ajax
+// fetch()로도 구현 가능(일부 브라우저 또는 하위 버전의 스크립트에서 호환X)
+// 비동기 방식으로 페이지의 일부 정보를 갱신할 수 있는 기술
+// -> jQuery.ajax() 메소드를 활용
+let pageNum = 1;
+const query = document.querySelector(".query");
+const container = document.querySelector(".container");
+const searchBox = document.querySelector(".search-box");
+const footer = document.querySelector("footer");
+const searchImg = document.querySelector(".search-img")
+let firstCheck = true;
+searchBox.addEventListener("submit", e => {
+    e.preventDefault(); // submit의 기본 동작 무시
+    searchImg.setAttribute("style", "display : none");
+    if (firstCheck) {
+        const nextButton = document.createElement("button");
+        nextButton.setAttribute("class", "nextButton");
+        nextButton.addEventListener("click", e => {
+            searchRequest(searchText, ++pageNum);
+        });
+
+        footer.append(nextButton);
+        firstCheck = false;
+    }
+    const searchText = `${query.value}`;
+    if (searchText !== "") {
+        pageNum = 1;
+        searchRequest(searchText);
+    }
+});
+function searchRequest(searchText, pageNum) {
+    $.ajax({
+        "url": `http://dapi.kakao.com/v3/search/book?query=${searchText}&page=${pageNum}&size=25&target=title`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": "KakaoAK 205762e8ca1e0563194b28ba858ae453"
+        },
+    }).done(function (response) {
+        console.log(response);
+        printPage(response);
+    });
+}
+function printPage(response) {
+    for (let i = 0; i < response.documents.length; i++) {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        const h4 = document.createElement("h4");
+        const p1 = document.createElement("p");
+        const span1 = document.createElement("span");
+        const p2 = document.createElement("p");
+        const span2 = document.createElement("span");
+        const span3 = document.createElement("span");
+        div.setAttribute("class", "result-card");
+        img.setAttribute("class", "book-img");
+        img.setAttribute("src", `${response.documents[i].thumbnail}`);
+        h4.setAttribute("class", "book-title");
+        p1.setAttribute("class", "book-description");
+        span1.setAttribute("class", "price");
+        p2.setAttribute("class", "book-info");
+        span2.setAttribute("class", "author");
+        span3.setAttribute("class", "publisher");
+        h4.innerText = `${response.documents[i].title}`;
+        const description = `${response.documents[i].contents}`;
+        if (description.length >= 15) {
+            p1.innerText = description.substring(0, 15) + "...";
+        }
+        else {
+            p1.innerText = description;
+        }
+        span1.innerText = `${response.documents[i].price}` + "원";;
+        span2.innerText = `${response.documents[i].authors[0]}` + " | ";
+        span3.innerText = `${response.documents[i].publisher}`;
+        div.append(img);
+        div.append(h4);
+        p2.append(span2);
+        p2.append(span3);
+        div.append(p2);
+        div.append(span1);
+        div.append(p1);
+        container.append(div);
+    }
+    // 페이지 정보 변수로 빼기
+    // 가져온 정보들을 엘리먼트 안에 붙이기만 하면 됨
+    // 카드들은 마음에 드는 디자인 참고를 해서 꾸며보세요~
+    // container 안에
+    //     <div class = "result-card">
+    //     <img class = "book-img" src = "book.png">
+    //     <h4 class = "book-title">도서 제목</h4>
+    //     <p class = "book-description">도서상세정보</p>
+    //     <span class = "price">1000원</span>
+    //     <p class = "book-info">
+    //         <span class = "author">저자</span><span class = "publisher">출판사</span>
+    //     </p>
+    // </div>
+}
---
+++ omok/omok.js
@@ -0,0 +1,146 @@
+let turn = 1;
+let win = 0;
+const size = 15;
+const mark = [];
+const table = document.querySelector("table");
+
+for (let x = 1; x <= 2; x++) {
+    const tbody = document.createElement("tbody");
+    tbody.setAttribute("id", `tbody-${x}`)
+
+    for (let i = 0; i < size; i++) {
+        const markIn = [];
+        mark.push(markIn);
+        const tr = document.createElement("tr");
+        tr.setAttribute("id", `tr-${i}`)
+        for (let j = 0; j < size; j++) {
+            const check = 0;
+            markIn.push(check);
+
+            const td = document.createElement("td");
+            td.setAttribute("class", `td${x}`)
+            td.setAttribute("id", `td-${i},${j}`);
+
+            if (x === 1 && j !== size - 1 && i !== size - 1) {
+                td.addEventListener("click", e => {
+                    if (mark[i][j] === 0) {
+                        const img = document.createElement("img");
+                        img.setAttribute("class", "baduk-img");
+                        if (turn == 1) {
+                            img.setAttribute("src", "white.png");
+                        }
+
+                        else {
+                            img.setAttribute("src", "black.png");
+                        }
+
+                        td.append(img);
+                        mark[i][j] = turn;
+                        checkWin();
+                    }
+                });
+            }
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+
+    table.append(tbody);
+}
+
+function checkWin() {
+    checkHori();
+    checkverti();
+    checkCross();
+
+    if (win !== 0) {
+        if (win === 1) {
+            alert(`하얀돌이 이겼습니다!`);
+        }
+
+        else {
+            alert(`검은돌이 이겼습니다!`);
+
+        }
+    }
+
+    turn = turn === 1 ? 2 : 1;
+}
+
+function checkHori() {
+    for (let i = 0; i < size; i++) {
+        let count = 0;
+        for (let j = 0; j < size; j++) {
+            if (mark[i][j] === turn) {
+                count++;
+            }
+
+            else {
+                count = 0;
+            }
+
+            if (count === 5) {
+                win = turn;
+            }
+        }
+    }
+}
+
+function checkverti() {
+    for (let i = 0; i < size; i++) {
+        let count = 0;
+        for (let j = 0; j < size; j++) {
+            if (mark[j][i] === turn) {
+                count++;
+            }
+
+            else {
+                count = 0;
+            }
+
+            if (count === 5) {
+                win = turn;
+            }
+        }
+    }
+}
+
+function checkCross() {
+    let count = 0;
+    for (let j = 0; j < size; j++) {
+        for (let k = 0; k < size; k++) {
+            for (let i = 0; i < size; i++) {
+                if (i + j < size && i + k < size && mark[i + j][i + k] === turn) {
+                    count++;
+                }
+
+                else {
+                    count = 0;
+                }
+
+                if (count === 5) {
+                    win = turn;
+                }
+            }
+        }
+    }
+
+    count = 0;
+    for (let j = 0; j < size; j++) {
+        for (let k = 0; k < size; k++) {
+            for (let i = 0; i < size; i++) {
+                if (i - j >= 0 && size - 1 - i - k >= 0 && mark[i - j][size - 1 - i - k] === turn) { // 51 42 31 20 // 61 52 // 52 43
+                    count++;
+                }
+
+                else {
+                    count = 0;
+                }
+
+                if (count === 5) {
+                    win = turn;
+                }
+            }
+        }
+    }
+}
