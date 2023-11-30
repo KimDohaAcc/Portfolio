dd508f7f6892375376f7166d31b967e3f722eafa
KimDohaAcc <8772158@naver.com>
2023-12-01 05:09:46 +0900

    프로젝트, interest 추가

--- resources/script/index.js
+++ resources/script/index.js
@@ -56,6 +56,8 @@ function addProject(project, targetContainer, imageSize, imagePosition) {
     const period = document.createElement("span");
     
     article.setAttribute("class", project.title);
+    article.style.height = "40vh";
+    article.style.position = "relative";
     a.setAttribute("href", project.link);
     a.setAttribute("target", "_blank");
     projectImg.setAttribute("class", "project-img");
@@ -65,8 +67,14 @@ function addProject(project, targetContainer, imageSize, imagePosition) {
     description.className = "description";
     description.innerText = project.description;
     skills.setAttribute("class", "skills");
+    skills.style.display = "flex";
+    skills.style.flexFlow = "wrap row";
+    skills.style.height = "fit-content";
     period.className = "period";
     period.innerText = `${project.period[0]} ~ ${project.period[1]}`;
+    period.style.position = "absolute";
+    period.style.bottom = "0";
+
 
     a.append(projectImg);
     article.append(a);
@@ -75,6 +83,7 @@ function addProject(project, targetContainer, imageSize, imagePosition) {
     project.skills.forEach(str => {
         const skill = document.createElement("span");
         skill.innerText = str;
+        skill.style.marginBottom = "2%";
         skills.append(skill);
     });
     article.append(skills);
@@ -104,7 +113,7 @@ const bookSearch = new Project(
     "Book Search",
     "Book Search and Inquiry Service",
     ['VanilaJS', 'jQeury', 'Kakao Search API'],
-    ['2023.03.13', "2023.03.13"]
+    ['2023.03.13', '2023.03.13']
 );
 
 addProject(bookSearch, "frontend", "contain", "center");
@@ -115,62 +124,97 @@ const omok = new Project(
     "Omok",
     "Omok Game",
     ['VanilaJS'],
-    ['2023.03.12', "2023.03.12"]
+    ['2023.03.12', '2023.03.12']
 );
 
 addProject(omok, "frontend", "contain", "center");
 
-const atm = new Project(
-    "https://github.com/dohakim0205/ATM.git",
-    "/resources/images/ATM.jpg",
-    "DOHA BANK",
-    "Console ATM (Java mini profect)",
-    ['JAVA'], 
-    ["2023.03.13"] 
+
+const HealthPanda = new Project(
+    "https://github.com/KimDohaAcc/HealthPanda.git",
+    "/resources/images/healthPanda.png",
+    "Health-Panda",
+    "헬스장 웹/챗봇 서비스 (Web Project)",
+    ['JAVA','Spring','Vue','mySQL','JS', 'node.js', 'AWS', 'kakao-openbuilder'],
+    ["2023.10.17", "2023.11.23"]
 );
 
-addProject(atm, "backend", "contain", "center");
+addProject(HealthPanda, "backend", "contain", "center");
 
-const RPG = new Project(
-    "https://github.com/dohakim0205/RPG-GAME.git",
-    "/resources/images/RPG-GAME.png",
-    "RPG",
-    "Console RPG (Java mini profect)",
-    ['JAVA'],
-    ["2023.03.23"]
+const butok = new Project(
+    "https://github.com/KimDohaAcc/Butok.git",
+    "/resources/images/toktok.png",
+    "Butok",
+    "부동산 전월세 실거래가 조회 사이트 (Web Project)",
+    ['JAVA','Spring','mySQL','JSP','JS', 'AWS'],
+    ["2023.04.27", "2023.05.15"]
 );
 
-addProject(RPG, "backend", "contain", "center");
+addProject(butok, "backend", "contain", "center");
 
-const Pokemon = new Project(
-    "https://github.com/dohakim0205/Pokemon.git",
-    "/resources/images/Pokemon.png",
-    "Pokemon",
-    "Console Pokemon (Java mini profect)",
-    ['JAVA'],
-    ["2023.03.26"]
+const GamjaMarket = new Project(
+    "https://github.com/KimDohaAcc/GamjaMarket.git",
+    "/resources/images/potato.png",
+    "Gamja-Market",
+    "동네 중고 거래 사이트 (Web Project)",
+    ['JAVA','mySQL','JSP','JS', 'AWS'],
+    ["2023.04.11", "2023.04.24"]
 );
 
-addProject(Pokemon, "backend", "contain", "center");
+addProject(GamjaMarket, "backend", "contain", "center");
+
+const RentCar = new Project(
+    "https://github.com/KimDohaAcc/Rentcar.git",
+    "/resources/images/RentCar.png",
+    "RentCar",
+    "Console RentCar (Java mini project)",
+    ['JAVA', 'SQL','JSP','JS'],
+    ["2023.04.07", "2023.04.07"]
+);
+
+addProject(RentCar, "backend", "contain", "center");
 
 const Poly_RPG_Console_Game = new Project(
-    "https://github.com/dohakim0205/Poly-RPG-Console-Game.git",
+    "https://github.com/KimDohaAcc/Poly-RPG-Console-Game.git",
     "/resources/images/Poly_RPG_Console_Game.png",
     "Poly RPG Console Game",
-    "Poly_RPG_Console_Game (Java mini profect)",
+    "Poly_RPG_Console_Game (Java mini project)",
     ['JAVA'],
-    ["2023.03.28"]
+    ["2023.03.28", "2023.03.28"]
 );
 
 addProject(Poly_RPG_Console_Game, "backend", "contain", "center");
 
-const RentCar = new Project(
-    "https://github.com/dohakim0205/Rentcar.git",
-    "/resources/images/RentCar.png",
-    "RentCar",
-    "Console RentCar (Java mini profect)",
-    ['JAVA', 'SQL','JSP','JS'],
-    ["2023.04.07"]
+const Pokemon = new Project(
+    "https://github.com/KimDohaAcc/Pokemon.git",
+    "/resources/images/Pokemon.png",
+    "Pokemon",
+    "Console Pokemon (Java mini project)",
+    ['JAVA'],
+    ["2023.03.26", "2023.03.26"]
 );
 
-addProject(RentCar, "backend", "contain", "center");
+addProject(Pokemon, "backend", "contain", "center");
+
+const RPG = new Project(
+    "https://github.com/KimDohaAcc/RPG-GAME.git",
+    "/resources/images/RPG-GAME.png",
+    "RPG",
+    "Console RPG (Java mini project)",
+    ['JAVA'],
+    ["2023.03.23", "2023.03.23"]
+);
+
+addProject(RPG, "backend", "contain", "center");
+
+const atm = new Project(
+    "https://github.com/KimDohaAcc/ATM.git",
+    "/resources/images/ATM.jpg",
+    "DOHA BANK",
+    "Console ATM (Java mini project)",
+    ['JAVA'], 
+    ["2023.03.13", "2023.03.13"] 
+);
+
+addProject(atm, "backend", "contain", "center");
+
