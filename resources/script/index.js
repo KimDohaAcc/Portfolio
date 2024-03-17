0fd062425d750eff84b42748275fffdc50276b18
KimDohaAcc <8772158@naver.com>
2024-03-17 21:20:59 +0900

    수정

--- resources/script/index.js
+++ resources/script/index.js
@@ -124,7 +124,7 @@ function addProject(project, targetContainer, imageSize, imagePosition) {
     const detailReview = document.createElement("p");
 
     siteLinkTitle.setAttribute("class", "detail-title");
-    siteLinkTitle.innerText = "사이트\t\t";
+    siteLinkTitle.innerText = "Github\t\t";
     siteLink.setAttribute("class", "detail-content");
     siteLink.href = project.site;
     siteLink.innerText = project.site;
@@ -185,14 +185,12 @@ const HealthPanda = new Project(
     "헬스장 웹/챗봇 서비스 (Web Project)",
     ['JAVA','Spring','Vue','mySQL','JS', 'node.js', 'AWS', 'kakao-openbuilder'],
     ["2023.10.17", "2023.11.23"],
-    "https://healthpanda.site",
+    "https://github.com/KimDohaAcc/HealthPanda.git",
     "👤👤",
     "챗봇 - 챗봇 개발 환경 구축, 식단 조회 및 기록, 식단 추천\n웹 - https 배포, RDS 구축, JWT 토큰 적용,운동 영상 추천, 좋아요 및 팔로우",
-    "kakao-openbuilder 스킬 환경 구축\n\nREST API로 스킬 요청을 보낼 서버 선정에 고민이 있었습니다. 상시 가동되는 aws의 EC2가 아니라 요청이 들어올 때만 작동하는 aws lambda를 사용할 수 있다면 비용 절감 측면에서 더욱 적합할 것이라고 생각해 서버리스 컴퓨팅 서비스인 lambda로 스킬 환경 구축을 시도해 보았습니다. "
-    +"그러나 lambda는 별도의 서버 세팅 없이 함수로써 코드를 실행하기 때문에 함수에서 Springboot와 데이터베이스를 이용하려면 추가적으로 spring cloud와 RDS 연결 설정이 필요한 것을 알게 되었습니다. 덧붙여 lambda는 초기 응답 시간이 느리면 cold start 문제가 발생하고, 함수의 크기가 제한되어 있기 때문에 springboot 이용 시 많은 최적화가 필요했습니다. "
-    +"lambda의 활용은 비용적 측면에서 분명 유의미한 시도이지만, 짧은 프로젝트 기간과 springboot 기반 환경 요인을 고려하여 스킬 서버로 EC2를 채택하게 되었습니다."
+    "[kakao-openbuilder 스킬 환경 구축]\n\n서버 환경을 결정할 때는 비용과 효율을 고려해야 했습니다. 초기에는 Lambda를 선택하여 서버리스 컴퓨팅을 시도했지만, Lambda는 Spring Boot 및 데이터베이스와 같은 추가적인 설정이 필요했습니다. 또한, Lambda의 cold start 문제와 함수 크기 제한으로 인해 Spring Boot를 사용할 때는 많은 최적화가 필요했습니다. 이에 따라 짧은 프로젝트 기간과 Spring Boot 환경을 고려하여 EC2를 선택했습니다."
     + 
-    "\n\nAWS를 활용한 https 설정\n\n보안에 유리한 HTTPS 보안 서버를 구축하고자 하였습니다. 도메인을 발급하고 aws Route53으로 도메인 소유를 인증해준 뒤 ACM으로 SSL 인증서를 발급 받아 https 서버를 구축했습니다. 추가로 https(443), http 요청(8080)을 모두 https로 보내기 위하여 Load Balancer에 해당 규칙을 추가해 주었습니다."
+    "\n\n[AWS를 활용한 https 설정]\n\nHTTPS 보안 서버를 구축하기 위해 도메인 발급과 AWS의 Route53을 사용하여 도메인 소유를 인증하고, ACM으로 SSL 인증서를 발급 받았습니다. 또한, 모든 요청을 HTTPS로 리다이렉션하기 위해 Load Balancer에 규칙을 추가했습니다."
 );
 
 addProject(HealthPanda, "backend", "contain", "center");
@@ -204,13 +202,13 @@ const butok = new Project(
     "부동산 전월세 실거래가 조회 사이트 (Web Project)",
     ['JAVA','Spring','mySQL','JSP','JS', 'AWS'],
     ["2023.04.27", "2023.05.15"],
-    "http://butok.site",
+    "https://github.com/KimDohaAcc/Butok.git",
     "👤👤👤👤",
     "회원의 선호 기반 매물 추천, 회원 보안",
-    "Spring security 적용\n\n"
+    "[Spring security 적용]\n\n"
     + "Spring security 2.7 버전을 사용하여 비밀번호를 암호화 하였으며, 회원과 관리자의 역할을 나눠 관리자 페이지의 접근을 막았습니다. "
     + "그 과정에서 필요한 config 파일 작성 방법과 userDetail 상속 등을 익힐 수 있었으며, jsp에서 spring security tag library를 활용하여 간편하게 권한 확인을 할 수 있었습니다."
-    + "\n\njpa와 pageable을 활용한 pagination 처리\n\n"
+    + "\n\n[pageable을 활용한 pagination 처리]\n\nPageable을 이용하여 페이지네이션 처리를 구현하였습니다. 전월세 데이터가 많이 존재하는 경우 데이터를 효율적으로 관리하고 사용자에게 더 나은 경험을 제공할 수 있었습니다. 이를 통해 데이터 관리와 성능 최적화에 대한 이해를 높였습니다."
     + "..."
 );
 
@@ -223,11 +221,11 @@ const GamjaMarket = new Project(
     "동네 중고 거래 사이트 (Web Project)",
     ['JAVA','mySQL','JSP','JS', 'AWS'],
     ["2023.04.11", "2023.04.24"],
-    "http://gamja-market.site(현재 사용 불가)",
+    "https://github.com/KimDohaAcc/GamjaMarket.git",
     "👤👤👤👤",
     "판매자와 구매자 1:1 채팅, 자유게시판",
-    "firebase의 realtime-database를 활용한 채팅 구현\n\n"
-    + "실시간 데이터베이스를 이용하여 실시간 채팅 및 채팅 알람 기능을 구현하였습니다. ..."
+    "[firebase의 realtime-database를 활용한 채팅 구현]\n\n"
+    + "Firebase의 Realtime Database를 활용하여 실시간 채팅 및 채팅 알람 기능을 구현했습니다. 이를 통해 사용자들이 실시간으로 채팅할 수 있으며, 새로운 메시지가 도착하면 메인 화면에 알림을 표시했습니다. 이를 통해 실시간 데이터 처리 및 알림 시스템을 구축하면서 Message Listener에 대한 개념을 익혔습니다."
 );
 
 addProject(GamjaMarket, "backend", "contain", "center");
